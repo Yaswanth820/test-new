474e3c43a356f5da310d0f4541f8cd750a3b6891
csg33k <balajiyaswanth.v@gmail.com>
2021-10-29 15:51:48 +0530

    done for the day

--- server/node.js
+++ server/node.js
@@ -6,6 +6,15 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.listen(PORT(), ()=>{
+app.listen(PORT, ()=>{
     console.log('Server started');
+});
+
+app.get("/",(req,res)=>{
+    res.send('<h1>hello</h1>');
+});
+
+app.post("/",(req, res) => {
+    console.log(req.body);
+    res.status(200).send('Image Received');
 });
