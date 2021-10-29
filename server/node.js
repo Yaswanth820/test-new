16ecf0e9667e2da15fd8d6a97084e3fc4610b49e
csg33k <balajiyaswanth.v@gmail.com>
2021-10-29 12:27:45 +0530

    understanding ports

--- server/node.js
+++ server/node.js
@@ -1,2 +1,11 @@
-const exp = require('express');
+const express = require('express');
 const cors = require('cors');
+const PORT = process.env.PORT || 8000;
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.listen(PORT(), ()=>{
+    console.log('Server started');
+});
