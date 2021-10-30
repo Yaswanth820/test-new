ce7c2e81f9b9925efa9d752d9ff8aaf21dbbd25f
csg33k <balajiyaswanth.v@gmail.com>
2021-10-30 14:48:37 +0530

    today

--- server/index.js
+++ server/index.js
@@ -1,20 +1,47 @@
 const express = require('express');
 const cors = require('cors');
-const PORT = process.env.PORT || 8000;
+const md5 = require('md5');
+const mongoose = require('mongoose');
 
+require('dotenv').config();
 const app = express();
 app.use(express.json());
 app.use(cors());
+const PORT = process.env.PORT || 8000;
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const connection = mongoose.connection;
+
+connection.once(`open`, ()=>{
+    console.log('Mongoose is running');
+});
 
 app.listen(PORT, ()=>{
-    console.log('Server started');
+    console.log('Server started at `http://localhost:8000/`');
 });
 
+var i = 0;
 app.get('/',(req, res)=>{
-    res.send('<h1>hello</h1>');
+    //res.send('<h1>hello</h1>');
+    res.send(`${i++}`);
 });
+
+// app.get('/:string',(req, res)=>{
+//     res.send(req.params.string)
+// });
 
 app.post('/',(req, res) => {
     console.log(req.body);
     res.status(200).send('Image Received');
+});
+
+app.post('/create', (req, res) => {
+    console.log(req.body);
+    const { name } = req.body;
+    res.status(200).send(`http://localhost:8000/${md5(name).slice(0, 6)}`);
+});
+
+app.get('/find/:string/', (req, res) => {
+    res.send(`Found something at ${req.path}`);
 });
